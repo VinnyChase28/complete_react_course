b51d9d9f4d74041a6e89a917e1d6ce3dfd57c907
Vincent  Gauthier <vincent@operto.com>
2019-08-10 12:57:04 -0700

    Assignment 1 complete - use a different template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,19 +5,54 @@ console.log('App.js is running!');
 //JSX Javascript XML
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Does this change?!!!'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'This is some text'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some Info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Vincent Gauthier'
+    ),
+    React.createElement(
+        'p',
+        null,
+        '28'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: New Westminster'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -2,7 +2,25 @@ console.log('App.js is running!')
 
 //JSX Javascript XML
 
-var template = <div><h1>Does this change?!!!</h1><p>This is some text</p></div>;
+var template = ( 
+ <div>
+    <h1>Indecision App</h1>
+    <p>This is some Info</p>
+    <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+    </ol>
+</div>
+);
+
+var templateTwo = (
+  <div>
+  <h1>Vincent Gauthier</h1>
+  <p>28</p>
+  <p>Location: New Westminster</p>
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
