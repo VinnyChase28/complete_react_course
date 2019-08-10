a01f4d5f84c45030d5186d592f4c490a2edfe0af
Vincent  Gauthier <vincent@operto.com>
2019-08-10 13:23:52 -0700

    fix user not defined by commenting out template2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,49 +4,35 @@ console.log('App.js is running!');
 
 //JSX Javascript XML
 var app = {
-  title: 'Indecision App',
-  sub_title: 'My First App'
+    title: 'Indecision App',
+    sub_title: 'Put your hands in the life of a computer'
 
 };
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    app.title
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.sub_title
-  )
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.sub_title
+    )
 );
 
-var userName = 'Vincent Gauthier';
-var userAge = 26;
-var userLocation = 'New Westminster';
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name
-  ),
-  React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  React.createElement(
-    'p',
-    null,
-    'Location: ',
-    user.location
-  )
-);
+// var userName = 'Vincent Gauthier'
+// var userAge = 26;
+// var userLocation = 'New Westminster'
+// var templateTwo = (
+//   <div>
+//   <h1>{user.name}</h1>
+//   <p>Age: {user.age}</p>
+//   <p>Location: {user.location}</p>
+//   </div>
+// );
 
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ console.log('App.js is running!')
 //JSX Javascript XML
 var app = {
     title: 'Indecision App',
-    sub_title: 'My First App',
+    sub_title: 'Put your hands in the life of a computer',
    
 };
 var template = ( 
@@ -13,16 +13,16 @@ var template = (
 </div>
 );
 
-var userName = 'Vincent Gauthier'
-var userAge = 26;
-var userLocation = 'New Westminster'
-var templateTwo = (
-  <div>
-  <h1>{user.name}</h1>
-  <p>Age: {user.age}</p>
-  <p>Location: {user.location}</p>
-  </div>
-);
+// var userName = 'Vincent Gauthier'
+// var userAge = 26;
+// var userLocation = 'New Westminster'
+// var templateTwo = (
+//   <div>
+//   <h1>{user.name}</h1>
+//   <p>Age: {user.age}</p>
+//   <p>Location: {user.location}</p>
+//   </div>
+// );
 
 var appRoot = document.getElementById('app');
 
