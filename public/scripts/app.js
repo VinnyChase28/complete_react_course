342ac16c5222c1ba62ff5674928672d4b442aadb
Vincent  Gauthier <vincent@operto.com>
2019-08-10 12:41:40 -0700

    Add a wrapping div to properly render with react

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,9 +5,18 @@ console.log('App.js is running!');
 //JSX Javascript XML
 
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'Does this change?!!!'
+  React.createElement(
+    'h1',
+    null,
+    'Does this change?!!!'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'This is some text'
+  )
 );
 var appRoot = document.getElementById('app');
 
--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ console.log('App.js is running!')
 
 //JSX Javascript XML
 
-var template = <p>Does this change?!!!</p>;
+var template = <div><h1>Does this change?!!!</h1><p>This is some text</p></div>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
