efa5d67022d87c4cbc6848b48117ecd28ff475d3
Vincent  Gauthier <vincent@operto.com>
2019-08-21 22:49:28 -0700

    first repeatable user interaction

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,45 +1,69 @@
 "use strict";
 
-// arguments object - no longer bound with arrow functions
+console.log("App.js is running!");
 
-var add = function add(a, b) {
-  //   console.log(arguments);
-  return a + b;
+// if statements
+// ternary operrators
+// logical and operator
+
+//only render the sub-title (and p tag) if subtitle exist - logical and operator
+// render new p tag - if options.length > 0 "Here are your options" "No options"
+
+//make sure you remember {app.subtitle && <p>{app.subtitle}</p>}
+//reads as "if we have app.subtitle then render the p tag"
+//and <p>{app.options.length > 0 ? 'Here are your options:': 'No options'}</p>
+//reads as if the length is greater than 0, print here are your options, otherwise no options
+
+//changed var to const. assigning variables with const ensures defensive programming
+
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your hands in the life of a computer",
+  options: ["One", "Two"]
 };
-console.log(add(55, 1, 1001));
+var template = React.createElement(
+  "div",
+  null,
+  React.createElement("h1", null, app.title),
+  app.subtitle && React.createElement("p", null, app.subtitle),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options:" : "No options"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement("li", null, "Item One"),
+    React.createElement("li", null, "Item Tthreeeeeeee")
+  )
+);
 
-//this keyword - no longer bound
+// this is the first repeatable user interaction in this course.
 
-var user = {
-  name: "Vince",
-  cities: ["Montreal", "Vancouver", "New York"],
-  printPlacesLived: function printPlacesLived() {
-    var _this = this;
-
-    return this.cities.map(function(city) {
-      return _this.name + " has lived in " + city + "!";
-    });
-  }
+var count = 0;
+var addOne = function addOne() {
+  console.log("addOne");
 };
-
-console.log(user.printPlacesLived());
-
-//Challenge area
-
-var multiplier = {
-  //numbers - array of numbers
-  //multiplyBy (value) - single number
-  //multiply (method) return a new array where the number has been multiplied
-  multiplyBy: 2,
-  numbers: [20, 40, 60], //done
-
-  multiply: function multiply() {
-    var _this2 = this;
-
-    return this.numbers.map(function(number) {
-      return number * _this2.multiplyBy;
-    });
-  }
+var minusOne = function minusOne() {
+  console.log("minusOne");
 };
+var reset = function reset() {
+  console.log("reset");
+};
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement("h1", null, "Count ", count),
+  React.createElement("button", { onClick: addOne }, "+1"),
+  React.createElement("button", { onClick: minusOne }, "-1"),
+  React.createElement("button", { onClick: reset }, "reset")
+);
 
-console.log(multiplier.multiply()); // [1, 2, 3] 2 [2, 4, 6]
+//Challenge
+//Make Button "-1" - setup minusOne function and register  - log 'minus One'
+//Make reset button 'reset' - setup reset function 'reset'
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(templateTwo, appRoot);
